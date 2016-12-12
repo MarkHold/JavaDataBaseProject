c536be4f7b1c512c1caffea1efddf21868efcd6f
markuslyconhold <ma223ik@student.lnu.se>
2016-12-12 14:50:58 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by markuslyconhold on 12/12/16.
+ */
+public class Main {
+
+    public static void main(String[]args){
+
+        System.out.println("iuo12u3");
+
+    }
+}
